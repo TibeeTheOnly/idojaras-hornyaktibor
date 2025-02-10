736679dd2380d742aca3a9c68f126fe4f066a778
Tibor Hornyák <hornyak.tibor@petrik.hu>
2025-02-10 14:48:02 +0100

    feat: 2. feladat

---
+++ NapiIdojaras.js
@@ -0,0 +1,15 @@
+class NapiIdojaras {
+    constructor(nap, maxHomerseklet, minHomerseklet, idojaras) {
+        this.nap = nap;
+        this.maxHomerseklet = maxHomerseklet;
+        this.minHomerseklet = minHomerseklet;
+        this.idojaras = idojaras;
+    }
+
+    toString() {
+        const napok = ["Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat"];
+        return `${napok[this.nap]}: Max: ${this.maxHomerseklet}°C, Min: ${this.minHomerseklet}°C, Időjárás: ${this.idojaras}`;
+    }
+}
+
+module.exports = NapiIdojaras;
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("Hello, world! This is the idojaras-hornyaktibor project.");
+const NapiIdojaras = require('./NapiIdojaras');
+
+const maiIdojaras = new NapiIdojaras(1, 25, 15, "napos");
+console.log(maiIdojaras.toString());
