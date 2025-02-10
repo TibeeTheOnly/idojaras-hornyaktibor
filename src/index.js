fe905e89f2cdfa3b3cd63bdc6bf86993fc7e1508
Tibor Hornyák <hornyak.tibor@petrik.hu>
2025-02-10 15:49:54 +0100

    fix: minden

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import NapiIdojaras from './NapiIdojaras.js';
 import { maiidojaras, beolvas } from './idojaraskezeles.js';
+import fs from 'fs/promises';
 
 const maiIdojaras = new NapiIdojaras(1, 25, 15, "napos");
 console.log(maiIdojaras.toString());
@@ -18,6 +19,18 @@ async function main() {
     });
 
     console.log(`A legmagasabb hőmérséklet - ${legmagasabbHomersekletuNap.toString()}`);
+
+    // Move the last element to the second position
+    if (idojarasLista.length > 1) {
+        const lastElement = idojarasLista.pop();
+        idojarasLista.splice(0, 0, lastElement);
+    }
+
+    const csvContent = idojarasLista.map(idojaras => 
+        `${idojaras.nap};${idojaras.minHomerseklet};${idojaras.maxHomerseklet};${idojaras.idojaras}`
+    ).join('\n');
+
+    await fs.writeFile('./src/idojaras-uj.csv', `nap;min;max;idojaras\n${csvContent}`, 'utf-8');
 }
 
 main();
